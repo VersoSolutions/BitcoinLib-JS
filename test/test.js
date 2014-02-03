3c2904adbb593fa235499ef008c33ca002c11648
Christopher T <ctrevisan@versocards.com>
2014-02-03 13:02:28 +0100

    Added the scrypt KDF

--- test/test.js
+++ test/test.js
@@ -60,6 +60,20 @@ test("PBKDF2", function(assert) {
     assert.deepEqual(Cryptography.PBKDF2(password, salt, iterations), key, "Key derivation");
 });
 
+test("scrypt", function (assert) {
+    expect(2);
+
+    var tv1 = [119,214,87,98,56,101,123,32,59,25,202,66,193,138,4,151,241,107,72,68,227,7,74,232,223,223,250,63,237,226,20,66,252,208,6,157,237,9,72,248,50,106,117,58,15,200,31,23,232,211,224,251,46,13,54,40,207,53,226,12,56,209,137,6];
+    var tv2 = [253,186,190,28,157,52,114,0,120,86,231,25,13,1,233,254,124,106,215,203,200,35,120,48,231,115,118,99,75,55,49,98,46,175,48,217,46,34,163,136,111,241,9,39,157,152,48,218,199,39,175,185,74,131,238,109,131,96,203,223,162,204,6,64];
+    var tv3 = [112,35,189,203,58,253,115,72,70,28,6,205,129,253,56,235,253,168,251,186,144,79,142,62,169,181,67,246,84,93,161,242,213,67,41,85,97,63,15,207,98,212,151,5,36,42,154,249,230,30,133,220,13,101,30,64,223,207,1,123,69,87,88,135];
+    var tv4 = [33,1,203,155,106,81,26,174,173,219,190,9,207,112,248,129,236,86,141,87,74,47,253,77,171,229,238,152,32,173,170,71,142,86,253,143,75,165,208,159,250,28,109,146,124,64,244,195,55,48,64,73,232,169,82,251,203,244,92,111,167,122,65,164];
+
+    assert.deepEqual(Cryptography.scrypt("", Verso.Encoding.utf8ToBytes(""), 16, 1, 1, 64*8), tv1);
+    assert.deepEqual(Cryptography.scrypt("password", Verso.Encoding.utf8ToBytes("NaCl"), 1024, 8, 16, 64*8), tv2);
+    //assert.deepEqual(Cryptography.scrypt("pleaseletmein", Verso.Encoding.utf8ToBytes("SodiumChloride"), 16384, 8, 1, 64*8), tv3);
+    //assert.deepEqual(Cryptography.scrypt("pleaseletmein", Verso.Encoding.utf8ToBytes("SodiumChloride"), 1048576, 8, 1, 64*8), tv4);
+});
+
 test("Shamir's secret sharing", function(assert) {
     expect(2);
 
@@ -119,5 +133,5 @@ test("Bitcoin: BIP 0032", function(assert) {
     assert.deepEqual(m0x.getPublic(true), Km0x, "m/0/2147483647': Public key (compressed)");
     assert.deepEqual(m0x123.getPrivate(), km0x123, "m/0/2147483647'/1/2147483646'/2: Private key");
     assert.deepEqual(m0x123.getPublic(true), Km0x123, "m/0/2147483647'/1/2147483646'/2: Public key (compressed)");
-    
+
 });
